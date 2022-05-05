74fc06ff9cc17674ee9f06dbca64a08ab1cc2f02
Ravi Byakod <rbyakod@gmail.com>
2022-05-05 08:25:53 -0700

    tested 2 models, workingon BERT next

---
+++ my-capstone/models/utils.py
@@ -0,0 +1,22 @@
+import re
+import string
+
+def process_tweet(tweet):
+  
+    tweet2 = re.sub(r'^RT[\s]+','', tweet)
+   
+    #remove hyperlinks
+    tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet2)
+    
+    #remove hashtag by removing the hast #sign from the word
+    tweet2 = re.sub(r'#','',tweet2)
+    
+    
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+    
+    tweets_clean = tweet.lower()
+    
+
+    return tweets_clean
+
